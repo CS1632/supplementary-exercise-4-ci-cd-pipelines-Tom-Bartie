b29a2e88d1ac2271cd65fb6da147876aba2d8188
TomBartie <63671640+TomBartie@users.noreply.github.com>
2022-12-05 22:39:23 -0500

    Update src code

--- src/main/java/edu/pitt/cs/RentACat.java
+++ src/main/java/edu/pitt/cs/RentACat.java
@@ -1,13 +1,25 @@
 package edu.pitt.cs;
 
-public class RentACat {
-	int val = 0;
-
-	public void incVal() {
-		val++;
+public interface RentACat {
+	public static RentACat createInstance() {
+		if (Config.getBuggyRentACat()) {
+			return new RentACatBuggy();
+		} else {
+			return new RentACatImpl();
+		}
 	}
 
-	public int getVal() {
-		return val;
-	}
+	public boolean returnCat(int id);
+
+	public boolean rentCat(int id);
+
+	public String listCats();
+
+	public boolean catExists(int id);
+
+	public boolean catAvailable(int id);
+
+	public Cat getCat(int id);
+
+	public void addCat(Cat c);
 }
--- src/test/java/edu/pitt/cs/RentACatTest.java
+++ src/test/java/edu/pitt/cs/RentACatTest.java
@@ -1,22 +1,330 @@
 package edu.pitt.cs;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
 
-import org.mockito.*;
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RentACatTest {
 
+	/**
+	 * The test fixture for this JUnit test. Test fixture: a fixed state of a set of
+	 * objects used as a baseline for running tests. The test fixture is initialized
+	 * using the @Before setUp method which runs before every test case. The test
+	 * fixture is removed using the @After tearDown method which runs after each
+	 * test case.
+	 */
+
+	RentACat r; // Object to test
+	Cat c1; // First cat object
+	Cat c2; // Second cat object
+	Cat c3; // Third cat object
+
+	@Before
+	public void setUp() throws Exception {
+		// Turn on automatic bug injection in the Cat class, to emulate a buggy Cat.
+		// Your unit tests should work regardless of these bugs.
+		Cat.bugInjectionOn = true;
+
+		// INITIALIZE THE TEST FIXTURE
+		// 1. Create a new RentACat object and assign to r
+		r = RentACat.createInstance();
+
+		// 2. Create an unrented Cat with ID 1 and name "Jennyanydots", assign to c1
+		// TODO: Fill in
+		c1 = Mockito.mock(Cat.class);
+		Mockito.when(c1.getName()).thenReturn("Jennyanydots");
+		Mockito.when(c1.getId()).thenReturn(1);
+		Mockito.when(c1.getRented()).thenReturn(false);
+		Mockito.when(c1.toString()).thenReturn("ID 1. Jennyanydots");
+
+		// 3. Create an unrented Cat with ID 2 and name "Old Deuteronomy", assign to c2
+		// TODO: Fill in
+		c2 = Mockito.mock(Cat.class);
+		Mockito.when(c2.getName()).thenReturn("Old Deuteronomy");
+		Mockito.when(c2.getId()).thenReturn(2);
+		Mockito.when(c2.getRented()).thenReturn(false);
+		Mockito.when(c2.toString()).thenReturn("ID 2. Old Deuteronomy");
+
+		// 4. Create an unrented Cat with ID 3 and name "Mistoffelees", assign to c3
+		// TODO: Fill in
+		c3 = Mockito.mock(Cat.class);
+		Mockito.when(c3.getName()).thenReturn("Mistoffelees");
+		Mockito.when(c3.getId()).thenReturn(3);
+		Mockito.when(c3.getRented()).thenReturn(false);
+		Mockito.when(c3.toString()).thenReturn("ID 3. Mistoffelees");
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// Not necessary strictly speaking since the references will be overwritten in
+		// the next setUp call anyway and Java has automatic garbage collection.
+		r = null;
+		c1 = null;
+		c2 = null;
+		c3 = null;
+	}
+
+	/**
+	 * Test case for Cat getCat(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: r has no cats.
+	 * Execution steps: Call getCat(2).
+	 * Postconditions: Return value is null.
+	 * </pre>
+	 */
+
 	@Test
-	public void testPapervsRock5() {
-		PaperPlayer p1 = Mockito.mock(PaperPlayer.class);
-		RockPlayer p2 = Mockito.mock(RockPlayer.class);
-		ScoreBoard sb = Mockito.mock(ScoreBoard.class);
-        RockPaperScissors rps = new RockPaperScissors(p1,p2,sb);
-        Mockito.when(p1.throwHand()).thenReturn(Hand.PAPER);
-        Mockito.when(p2.throwHand()).thenReturn(Hand.ROCK);
-        rps.play(5);
+	public void testGetCatNullNumCats0() {
+		assertEquals(null, r.getCat(2));
+	}
+
+	/**
+	 * Test case for Cat getCat(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 * Execution steps: Call getCat(2).
+	 * Postconditions: Return value is not null.
+	 *                 Returned cat has an ID of 2.
+	 * </pre>
+	 */
+
+	@Test
+	public void testGetCatNumCats3() {
+
+		// Preconditions
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+		// Check that getCat(2) returns 2
+		assertNotEquals(null, r.getCat(2));
+		assertEquals(2, r.getCat(2).getId());
+
+	}
+
+	/**
+	 * Test case for boolean catAvailable(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: r has no cats.
+	 * Execution steps: Call catAvailable(2).
+	 * Postconditions: Return value is false.
+	 * </pre>
+	 */
+
+	@Test
+	public void testCatAvailableFalseNumCats0() {
+		assertEquals(false, r.catAvailable(2));
+	}
+
+	/**
+	 * Test case for boolean catAvailable(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 *                c3 is rented.
+	 *                c1 and c2 are not rented.
+	 * Execution steps: Call catAvailable(2).
+	 * Postconditions: Return value is true.
+	 * </pre>
+	 */
+
+	@Test
+	public void testCatAvailableTrueNumCats3() {
+		// Preconditions
+		r.addCat(c1);
+		r.addCat(c2);
+		Mockito.when(c3.getRented()).thenReturn(true);
+		r.addCat(c3);
+		// test availible cat
+		assertEquals(true, r.catAvailable(2));
+	}
+
+	/**
+	 * Test case for boolean catAvailable(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 *                c2 is rented.
+	 *                c1 and c3 are not rented.
+	 * Execution steps: Call catAvailable(2).
+	 * Postconditions: Return value is false.
+	 * </pre>
+	 */
+
+	@Test
+	public void testCatAvailableFalseNumCats3() {
+		// Preconditions
+		r.addCat(c1);
+		Mockito.when(c2.getRented()).thenReturn(true);
+		r.addCat(c2);
+		r.addCat(c3);
+		// testing cat availible - expect false return
+		assertEquals(false, r.catAvailable(2));
+	}
+
+	/**
+	 * Test case for boolean catExists(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: r has no cats.
+	 * Execution steps: Call catExists(2).
+	 * Postconditions: Return value is false.
+	 * </pre>
+	 */
+
+	@Test
+	public void testCatExistsFalseNumCats0() {
+		assertEquals(false, r.catExists(2));
+	}
+
+	/**
+	 * Test case for boolean catExists(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 * Execution steps: Call catExists(2).
+	 * Postconditions: Return value is true.
+	 * </pre>
+	 */
+
+	@Test
+	public void testCatExistsTrueNumCats3() {
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+		assertEquals(true, r.catExists(2));
+	}
+
+	/**
+	 * Test case for String listCats().
+	 * 
+	 * <pre>
+	 * Preconditions: r has no cats.
+	 * Execution steps: Call listCats().
+	 * Postconditions: Return value is "".
+	 * </pre>
+	 */
+
+	@Test
+	public void testListCatsNumCats0() {
+		assertEquals("", r.listCats());
+	}
+
+	/**
+	 * Test case for String listCats().
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 * Execution steps: Call listCats().
+	 * Postconditions: Return value is "ID 1. Jennyanydots\nID 2. Old
+	 *                 Deuteronomy\nID 3. Mistoffelees\n".
+	 * </pre>
+	 */
+
+	@Test
+	public void testListCatsNumCats3() {
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+		assertEquals("ID 1. Jennyanydots\nID 2. Old Deuteronomy\nID 3. Mistoffelees\n", r.listCats());
+	}
+
+	/**
+	 * Test case for boolean rentCat(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: r has no cats.
+	 * Execution steps: Call rentCat(2).
+	 * Postconditions: Return value is false.
+	 * </pre>
+	 */
+
+	@Test
+	public void testRentCatFailureNumCats0() {
+		assertEquals(false, r.rentCat(2));
+	}
+
+	/**
+	 * Test case for boolean rentCat(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 *                c2 is rented.
+	 * Execution steps: Call rentCat(2).
+	 * Postconditions: Return value is false.
+	 *                 c1.rentCat(), c2.rentCat(), c3.rentCat() are never called.
+	 * </pre>
+	 * 
+	 * Hint: See sample_code/mockito_example/NoogieTest.java in the course
+	 * repository for an example of behavior verification. Refer to the
+	 * testBadgerPlayCalled method.
+	 */
+
+	@Test
+	public void testRentCatFailureNumCats3() {
+		r.addCat(c1);
+		Mockito.when(c2.getRented()).thenReturn(true);
+		r.addCat(c2);
+		r.addCat(c3);
+		assertEquals(false, r.rentCat(2));
+		verify(c1, Mockito.never()).rentCat();
+		verify(c2, Mockito.never()).rentCat();
+		verify(c3, Mockito.never()).rentCat();
+
+	}
+
+	/**
+	 * Test case for boolean returnCat(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: r has no cats.
+	 * Execution steps: Call returnCat(2).
+	 * Postconditions: Return value is false.
+	 * </pre>
+	 */
+
+	@Test
+	public void testReturnCatFailureNumCats0() {
+		assertEquals(false, r.returnCat(2));
+	}
+
+	/**
+	 * Test case for boolean returnCat(int id).
+	 * 
+	 * <pre>
+	 * Preconditions: c1, c2, and c3 are added to r using addCat(Cat c).
+	 *                c2 is rented.
+	 * Execution steps: Call returnCat(2).
+	 * Postconditions: Return value is true.
+	 *                 c2.returnCat() is called exactly once.
+	 *                 c1.returnCat() and c3.returnCat are never called.
+	 * </pre>
+	 * 
+	 * Hint: See sample_code/mockito_example/NoogieTest.java in the course
+	 * repository for an example of behavior verification. Refer to the
+	 * testBadgerPlayCalled method.
+	 */
+
+	@Test
+	public void testReturnCatNumCats3() {
+		r.addCat(c1);
+		Mockito.when(c2.getRented()).thenReturn(true);
+		r.addCat(c2);
+		r.addCat(c3);
+		assertEquals(true, r.returnCat(2));
+		verify(c2, Mockito.times(1)).returnCat();
+		verify(c1, Mockito.never()).returnCat();
+		verify(c3, Mockito.never()).returnCat();
+
 	}
 }
